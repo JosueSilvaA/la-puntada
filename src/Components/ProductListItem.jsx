0aa415a64d4869c7736d3c87416facb18a338b04
josue <josuemarkcs@gmail.com>
2020-07-19 15:07:13 -0600

    Funcionalidad de Mostrar Productos Escolares completada

--- src/Components/ProductListItem.jsx
+++ src/Components/ProductListItem.jsx
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
-import { Grid } from '@material-ui/core'
+import { 
+    Grid,
+    Card,
+    CardHeader,
+    CardContent,
+    Typography
+    } 
+from '@material-ui/core'
+
 
 const ProductListItem = ({product}) =>{
     return(
         <>
-            <Grid item xs={12} md ={6}>
-                <h1>{product.nombre}</h1>
+            <Grid item  xs={12} md ={4} style={{padding:'2%'}}>
+                <Card>
+                    <CardHeader
+                        title={product.nombre}
+                        subheader={product.creada}
+                    />
+                    <img
+                        src={product.imgProducto}
+                        title="Paella dish"
+                        style={{backgroundPosition:'center center',backgroundSize:'cover',width:'100%'}}
+                    />
+                    <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            Descripcion : {product.descripcion}
+                            <br/>
+                            Color : {product.color}
+                            <br/>
+                            Marca : {product.marca}
+                            <br/>
+                            Tipo Producto : {product.tipoUtil}
+                            <br/>
+                            Precio : {product.precio}
+                        </Typography>
+                    </CardContent>
+                </Card>
             </Grid>
+    
         </>
     )
 }
--- src/Pages/Catalogue.jsx
+++ src/Pages/Catalogue.jsx
@@ -1,40 +1,40 @@
-import React, { useEffect, useState} from 'react'
-import Navbar from '../Components/Navbar'
-import { Grid , Divider } from '@material-ui/core'
-import '../Styles/Catalogue.css'
-import CatalogueController from '../Controllers/CatologueController'
-import ProductListItem from '../Components/ProductListItem'
+import React, { useEffect, useState } from "react";
+import Navbar from "../Components/Navbar";
+import { Grid, Divider } from "@material-ui/core";
+import "../Styles/Catalogue.css";
+import CatalogueController from "../Controllers/CatologueController";
+import ProductListItem from "../Components/ProductListItem";
 
+const Catalogue = () => {
+  const [ProductList, setProductList] = useState([0]);
 
+  const getProductos = async () => {
+    const catalogo = new CatalogueController();
+    const products = await catalogo.getAllProducts();
 
-const Catalogue = () =>{
+    setProductList(products);
+  };
 
-    const [ProductList, setProductList] = useState([0])
-
-    const getProductos = async () =>{
-        const catalogo = new CatalogueController()
-        const products = await catalogo.getAllProducts();
-
-        setProductList(products);
-        console.log(products);
+  useEffect(() => {
+    if (ProductList[0] === 0) {
+      getProductos();
     }
+  });
 
-    useEffect(() => {
-        if(ProductList[0] === 0){
-            getProductos()
-        }
-    })
-
-    return (
-        <>
-            <Navbar pageName={'Catalogo'} goBack/>
-
-            <Grid container spacing={5} className='mt-3'>
-                {ProductList.map(product =>(<ProductListItem key={product.nombre} product={product}/>))}
-            </Grid>
-        </>
-    )
-}
-
+  return (
+    <>
+      <Navbar pageName={"Catalogo"} goBack />
+      <Grid className='mt-2' style={{width:'100%',margin:'0px',padding:'2%'}}>
+        <h5 style={{textAlign:'center'}}>Productos Escolares</h5> 
+        <hr/> 
+      </Grid>
+      <Grid container spacing={2} className=" contenedor-catalogo" style={{width:'100%',margin:'0px',padding:'2%'}}>
+        {ProductList.map(product => (
+          <ProductListItem key={product._id} product={product} />
+        ))}
+      </Grid>
+    </>
+  );
+};
 
 export default Catalogue;
