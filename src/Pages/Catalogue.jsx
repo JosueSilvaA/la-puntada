1bd1cdc4ab8dcebb0be5b321530d9102c06405c3
josue <josuemarkcs@gmail.com>
2020-07-25 20:10:17 -0600

    Barra de busqueda en pagina catalogo

--- src/Pages/Catalogue.jsx
+++ src/Pages/Catalogue.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../Components/Navbar';
-import { Grid } from '@material-ui/core';
 import "../Styles/Catalogue.css";
 import CatalogueController from "../Controllers/CatologueController";
 import ProductListItemSchool from "../Components/ProductListItemSchool";
 import ProductListItemTextil from '../Components/ProductListItemTextil'
+import SearchProduct from '../Components/SearchProducts'
+import { Grid, Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const Catalogue = () => {
   const [ProductListSchool, setProductListSchool] = useState([0]);
   const [ProductListTextil,setProductListTextil] =  useState([0]);
+  const [productSelect, setproductSelect] = useState({type:{escolar:false,textil:false,variado:false},product:{}});
 
   const getProductosSchool = async () => {
     const catalogo = new CatalogueController();
@@ -24,6 +28,37 @@ const Catalogue = () => {
     }
   });
 
+  const selectProduct = (dataProduct) => {
+    if(dataProduct.tipoUtil !== undefined){
+      setproductSelect(
+        {
+          type:{
+            escolar:true,textil:false,variado:false
+          },
+          product:dataProduct
+        }
+      )
+    }else if(dataProduct.tipoTextil !== undefined){
+      setproductSelect(
+        {
+          type:{
+            escolar:false,textil:true,variado:false
+          },
+          product:dataProduct
+        }
+      )
+    }else if (dataProduct.tipoVariado !== undefined){
+      setproductSelect(
+        {
+          type:{
+            escolar:false,textil:false,variado:true
+          },
+          product:dataProduct
+        }
+      )
+    }
+  };
+
   return (
     <>
       <Navbar pageName={"Catalogo"} goBack />
@@ -33,6 +68,81 @@ const Catalogue = () => {
         spacing={2}
         className = 'contenedor-catalogo'
       > 
+        <SearchProduct selectProduct={selectProduct}/>
+        {productSelect.type.escolar && (
+        <Grid container style={{display:'flex',justifyContent:'center'}}>
+          <Grid item xs={12} sm={6} md={4} lg={3} style={{padding:'10px'}}>
+            <Card style={{cursor:"default"}}>
+              <CardHeader
+                action={
+                  <IconButton aria-label="settings">
+                    <MoreVertIcon />
+                  </IconButton>
+                }
+                title={productSelect.product.nombre}
+                subheader={productSelect.product.creada}
+              />
+              <img
+                src={productSelect.product.imgProducto}
+                title="Paella dish"
+                className = 'imagen-tarjeta'
+                alt={productSelect.product.nombre}
+              />
+              <CardContent>
+                <Typography variant="button" color="textSecondary" component="p" style={{fontWeight:"bold"}}>
+                  Descripcion : {productSelect.product.descripcion}
+                  <br />
+                  Color : {productSelect.product.color}
+                  <br />
+                  Marca : {productSelect.product.marca}
+                  <br />
+                  Tipo Producto : {productSelect.product.tipoUtil}
+                  <br />
+                  Precio : {productSelect.product.precio} lps
+                  <br/>
+                  Cantidad : {productSelect.product.cantidad}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+        )}
+        {productSelect.type.textil && (
+          <Grid container style={{display:'flex',justifyContent:'center'}}>
+            <Grid item  xs={12} sm={6} md={4} lg={3}>
+                <Card style={{cursor:"default"}}>
+                    <CardHeader
+                        action={
+                            <IconButton aria-label="settings">
+                              <MoreVertIcon />
+                            </IconButton>
+                          }
+                        title={productSelect.product.nombre}
+                        subheader={productSelect.product.creada}
+                    />
+                    <img
+                        src={productSelect.product.imgProducto}
+                        title="Paella dish"
+                        className = 'imagen-tarjeta'
+                        alt={productSelect.product.nombre}
+                    />
+                    <CardContent>
+                        <Typography variant="button" color="textSecondary" component="p" style={{fontWeight:"bold"}}>
+                            Descripcion : {productSelect.product.descripcion}
+                            <br/>
+                            Color : {productSelect.product.color}
+                            <br/>
+                            Precio : {productSelect.product.precio} lps
+                            <br/>
+                            Tipo producto : {productSelect.product.tipoTextil}
+                            <br/>
+                            Cantidad : {productSelect.product.cantidad}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+          </Grid>
+        )}
         <Grid className="mt-2 contenedor-titulo">
           <h5 className="titulo-categoria">Productos Escolares</h5>
           <hr />
